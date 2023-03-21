a3c28b9a482bb58ea5fef82c6d233d346c270024
MahimPranto <mahim.pranto@gmail.com>
2023-03-21 16:16:21 +0600

    solved till 5th problem

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ for (var i=0; i<10; i++){
 for (var i=1; i<=100; i+=2){
     document.write(i);
 }
+
+//Coding challenge #3: Print the multiplication table with 7
+for (var i=1; i<=10; i++){
+    document.write("7*"+i+"="+i*7+"<br>");
+}
+
+//Coding challenge #4: Print all the multiplication tables with numbers from 1 to 10
+for (var i=1; i<=10; i++){
+    for (var j=1; j<=10; j++){
+        document.write(i+"*"+j+"="+i*j+" ");  
+    }
+    document.write("<br>");
+}
+
+//Coding challenge #5: Calculate the sum of numbers from 1 to 10
+var sum=0;
+
+for (var i=1; i<=10; i++){
+    sum+=i;
+}
+document.write(sum);
