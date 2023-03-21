db00ced2b0c9c3c9955d8ec831cd98b4173ac799
MahimPranto <mahim.pranto@gmail.com>
2023-03-21 15:59:21 +0600

    added html and js file

---
+++ script.js
@@ -0,0 +1,9 @@
+//Coding challenge #1: Print numbers from 1 to 10
+for (var i=0; i<10; i++){
+    document.write(i+1);
+}
+
+//Coding challenge #2: Print the odd numbers less than 100
+for (var i=1; i<=100; i+=2){
+    document.write(i);
+}
